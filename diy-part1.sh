07ab1f4a38f1eec87524c123f70e66b692a608bd
zlyuan99 <zlyuan@qq.com>
2021-12-14 17:42:01 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -18,5 +18,5 @@
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 
 # add tizi
-sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
-sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
+sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
+sed -i '$a src-git small https://github.com/kenzok8/small'>>feeds.conf.default
